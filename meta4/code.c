4c71e3abaaeccb30ff40b2158c09a3e48c95b435
Marco Pais <marco.cruz.pais@gmail.com>
2021-12-16 17:49:21 +0000

    tirei \n do main

--- meta4/code.c
+++ meta4/code.c
@@ -9,7 +9,20 @@ extern globalTable *Head; // cabeça das tabelas de símbolos
 void geraCode(no_ast *atual){
     globalTable *aux = Head;
 
+    // primeiro adiciona as variáveis globais
     while(aux){
+        if(!aux->func){
+            if(strcmp(aux->type, "int") == 0){
+
+            }
+            else if(strcmp(aux->type, "float32") == 0){
+
+            }
+            else if(strcmp(aux->type, "bool") == 0){
+
+            }
+        }
+
         aux = aux->next;
     }
 }
